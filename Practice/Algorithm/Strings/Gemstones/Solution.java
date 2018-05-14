9bc59e0a725740282a116d85e8e44150ca3e7eb0
VizCharlie <developer.vizcharlie@gmail.com>
2018-05-14 15:19:11 +0530

    Create Solution.java

---
+++ Practice/Algorithm/Strings/Gemstones/Solution.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int gemstones(String[] arr) {
+        int len = arr.length, index;
+        
+        arr[0] = arr[0].replaceAll("(.)(?=.*\\1)", "");
+        
+        for(index = 1 ; index < len ; index++){            
+            arr[0] = arr[0].replaceAll("[^" + arr[index] + "]","");
+        }
+        
+        return arr[0].length();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            String arrItem = scanner.nextLine();
+            arr[i] = arrItem;
+        }
+
+        int result = gemstones(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
