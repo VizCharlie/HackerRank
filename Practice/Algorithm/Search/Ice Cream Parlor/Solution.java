d4c8871db148f3fe97937f6b314c0a25c45d5757
VizCharlie <developer.vizcharlie@gmail.com>
2018-05-27 21:44:24 +0530

    Create Solution.java

---
+++ Practice/Algorithm/Search/Ice Cream Parlor/Solution.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    static int[] icecreamParlor(int m, int[] arr) {
+        int len = arr.length, index_x, index_y;
+        int[] tempArr = new int[2];
+        
+        for(index_x = 0 ; index_x < len - 1 ; index_x++){
+            for(index_y = index_x + 1 ; index_y < len ; index_y++){
+                if(arr[index_x] + arr[index_y] == m ){
+                    tempArr[0] = index_x + 1;
+                    tempArr[1] = index_y + 1;
+                    break;
+                }
+            }
+        }
+                
+        return tempArr;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int m = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] arr = new int[n];
+
+            String[] arrItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int arrItem = Integer.parseInt(arrItems[i]);
+                arr[i] = arrItem;
+            }
+
+            int[] result = icecreamParlor(m, arr);
+
+            for (int i = 0; i < result.length; i++) {
+                bufferedWriter.write(String.valueOf(result[i]));
+
+                if (i != result.length - 1) {
+                    bufferedWriter.write(" ");
+                }
+            }
+
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
