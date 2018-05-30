d3a30f29e214b359fa44a3732f7a0703d00f6568
VizCharlie <developer.vizcharlie@gmail.com>
2018-05-30 14:11:31 +0530

    Create Solution.java

---
+++ Practice/Algorithm/Sorting/Counting Sort II/Solution.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] countingSort(int[] arr) {
+        int len = arr.length, index, counterIndex = 0;
+        int[] countArr = new int[len];     
+        
+        for(index = 0 ; index < len ; index++){
+            countArr[arr[index]]++;
+        }
+        
+        for(index = 0 ; index < len ; index++){
+            while(countArr[index]-- != 0){
+                arr[counterIndex++] = index;
+            }
+        }
+        
+        return arr;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] result = countingSort(arr);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
